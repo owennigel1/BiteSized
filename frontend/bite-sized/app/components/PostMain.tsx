61c434f09c160edf4ad659c4539069e255cd6ad4
codejiva <rambatilumbungpardja@gmail.com>
2024-07-27 22:58:00 +0700

    benerin video

--- frontend/bite-sized/app/components/PostMain.tsx
+++ frontend/bite-sized/app/components/PostMain.tsx
@@ -23,61 +23,58 @@ export default function ClientOnly({ post }: PostMainCompTypes) {
 
       observer.observe(postMainElement);
     }
-  }, []);
+  }, [post.id]);
 
   return (
-    <>
-      <div id={`PostMain-${post.id}`} className="flex border-b py-6">
-        <div className="cursor-pointer">
-          <img
-            className="rounded-full max-h-[60px]"
-            width="60"
-            src={post?.profile?.image}
-          />
+    <div id={`PostMain-${post.id}`} className="flex border-b py-6">
+      <div className="cursor-pointer">
+        <img
+          className="rounded-full max-h-[60px]"
+          width="60"
+          src={post?.profile?.image}
+          alt="Profile"
+        />
+      </div>
+
+      <div className="pl-3 w-full px-4">
+        <div className="flex items-center justify-between pb-0.5">
+          <Link href={`/profile/${post.profile.user_id}`}>
+            <span className="font-bold hover:underline cursor-pointer">
+              {post.profile.name}
+            </span>
+          </Link>
+
+          <button className="border text-[15px] px-[21px] py-0.5 border-[#7f79ac] text-[#7f79ac] hover:bg-[#ffeef2] font-semibold rounded-md">
+            Langganan
+          </button>
         </div>
-
-        <div className="pl-3 w-full px-4">
-          <div className="flex items-center justify-between pb-0.5">
-            <Link href={`/profile/${post.profile.user_id}`}>
-              <span className="font-bold hover:underline cursor-pointer">
-                {post.profile.name}
-              </span>
-            </Link>
-
-            <button className="border text-[15px] px-[21px] py-0.5 border-[#7f79ac] text-[#7f79ac] hover:bg-[#ffeef2] font-semibold rounded-md">
-              Langganan
-            </button>
-          </div>
-          <p className="text-[15px] pb-0.5 break-words md:max-w-[400px] max-w-[300px]">
-            {post.text}
-          </p>
-          <p className="text-[14px] text-gray-500 pb-0.5">
-            #jajanan #pedas #seblak #aura100000
-          </p>
-          <p className="text-[14px] pb-0.5 flex items-center font-semibold">
-            <ImMusic size="17" />
-            <span className="px-1">original sound - Seblak Level 10000</span>
-            <AiFillHeart size="20" />
-          </p>
-          <div className="mt-2.5 flex">
-            <div className="relative min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer">
-              <video
-                id={`video-${post.id}`}
-                loop
-                controls
-                muted
-                className="rounded-xl object-cover mx-auto h-full"
-                src={(post?.video_url)}
-              />
-              <img
-                className="absolute right-2 bottom-10"
-                width="90"
-                src="https://placehold.co/20" //! ini buat pp restonya
-              />
-            </div>
+        <p className="text-[15px] pb-0.5 break-words md:max-w-[400px] max-w-[300px]">
+          {post.text}
+        </p>
+        <p className="text-[14px] text-gray-500 pb-0.5">
+          #jajanan #pedas #seblak #aura100000
+          {/* ini tolong dibuat berdasarkan hasil scrapping-nya ya */}
+          {/* bisa juga berdasarkan penjual ngasih tagnya apa */}
+        </p>
+        <div className="mt-2.5 flex">
+          <div className="relative min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer">
+            <video
+              id={`video-${post.id}`}
+              loop
+              controls
+              muted
+              className="rounded-xl object-cover mx-auto h-full"
+              src={post?.video_url}
+            />
+            <img
+              className="absolute right-2 bottom-10"
+              width="90"
+              src="/images/BiteSized.png" //! dikasih watermark biar +100000 aura
+              alt="Watermark"
+            />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- frontend/bite-sized/app/page.tsx
+++ frontend/bite-sized/app/page.tsx
@@ -1,34 +1,59 @@
 "use client"
 
-import { useEffect } from "react"
 import MainLayout from "./layouts/MainLayout"
-import ClientOnly from "./components/ClientOnly"
 import PostMain from "./components/PostMain"
 
 export default function Home() {
+  const posts = [
+    //! ini buat contoh aja yakk
+    {
+      id: '123',
+      user_id: '666',
+      video_url: '/test-video-2.mp4',
+      text: 'Seblak paling pedes seantero semesta. Lokasi Gading Serpong. CUSSSS!!!',
+      created_at: 'ini tanggal dibuat',
+      profile: {
+        user_id: '666',
+        name: 'Seblak Uda Nay',
+        image: 'https://placehold.co/100',
+      },
+    },
+    {
+      id: '124',
+      user_id: '667',
+      video_url: '/test-video.mp4',
+      text: 'deskripsinya di sini',
+      created_at: 'ini tanggal dibuat',
+      profile: {
+        user_id: '667',
+        name: 'test ajaaa',
+        image: 'https://placehold.co/101',
+      },
+    },
+    {
+      id: '125',
+      user_id: '668',
+      video_url: '/test-video-3.mp4',
+      text: 'Ceker Midun! PROMO 1.100 PORSI GRATIS',
+      created_at: 'ini tanggal dibuat',
+      profile: {
+        user_id: '668',
+        name: 'Ceker Midun',
+        image: 'https://placehold.co/101',
+      },
+    },
+    
+  ];
 
   return (
     <>
       <MainLayout>
-        <div className="mt-[80px]  w-[calc(100%-90px)] max-w-[690px] ml-auto">
-          <ClientOnly>
-            <PostMain post={{ 
-              id: '123',
-              user_id: '666',
-              video_url: '/test-video.mp4', //! kalo mau pake video
-              //! image_url: '/test-image.jpg', //! kalo mau pake image
-              text: 'deskripsi',
-              created_at: 'ini tanggal dibuat',
-              profile: {
-                user_id: '666',
-                name: 'resto ABC',
-                image: 'https://placehold.co/100'
-              }
-             }}
-            />
-          </ClientOnly>
+        <div className="mt-[80px] w-[calc(100%-90px)] max-w-[690px] ml-auto overflow-y-auto max-h-screen">
+          {posts.map((post) => (
+            <PostMain key={post.id} post={post} />
+          ))}
         </div>
       </MainLayout>
     </>
-  )
+  );
 }
